d9b7e89b37e02acbd3d56c748a6dbd57bf0b2fb5
xmba15 <thba1590@gmail.com>
2019-11-05 13:12:45 +0900

    adding data

---
+++ data_loader/traffic_sign_data_loader.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class TrafficSignDataLoader(object):
+
+    def __init__(self):
+        pass
