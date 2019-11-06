f9d0ef6e309682cfb339e23212ba7104a6c2ec73
xmba15 <thba1590@gmail.com>
2019-11-07 04:05:10 +0900

    use he normal initializer

--- models/traffic_sign_net.py
+++ models/traffic_sign_net.py
@@ -7,31 +7,36 @@ tf.disable_v2_behavior()
 
 
 class TrafficSignNet(object):
-    def __init__(self, x, y, keep_prob, keep_prob_conv, n_out=43, mu=0, sigma=0.1, learning_rate=0.001):
+    def __init__(
+        self,
+        x,
+        y,
+        keep_prob,
+        keep_prob_conv,
+        n_out=43,
+        mu=0,
+        sigma=0.1,
+        learning_rate=0.001,
+    ):
         self.mu = mu
         self.sigma = sigma
+        initializer = tf.initializers.he_normal()
 
-        self.conv1_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 1, 32),
-                                mean=self.mu, stddev=self.sigma)
+        self.conv1_W = tf.get_variable(
+            name="conv1_w", shape=(3, 3, 1, 32), initializer=initializer
         )
-        self.conv1_b = tf.Variable(tf.zeros(32))
-        self.conv1 = (
-            tf.nn.conv2d(x, self.conv1_W, strides=[1, 1, 1, 1], padding="SAME")
-            + self.conv1_b
-        )
+
+        self.conv1 = tf.nn.conv2d(x, self.conv1_W, strides=[
+                                  1, 1, 1, 1], padding="SAME")
 
         self.conv1 = tf.nn.relu(self.conv1)
 
-        self.conv2_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 32, 32),
-                                mean=self.mu, stddev=self.sigma)
+        self.conv2_W = tf.get_variable(
+            name="conv2_w", shape=(3, 3, 32, 32), initializer=initializer
         )
-        self.conv2_b = tf.Variable(tf.zeros(32))
-        self.conv2 = (
-            tf.nn.conv2d(self.conv1, self.conv2_W, strides=[
-                         1, 1, 1, 1], padding="SAME")
-            + self.conv2_b
+
+        self.conv2 = tf.nn.conv2d(
+            self.conv1, self.conv2_W, strides=[1, 1, 1, 1], padding="SAME"
         )
 
         self.conv2 = tf.nn.relu(self.conv2)
@@ -41,61 +46,45 @@ class TrafficSignNet(object):
         )
         self.conv2 = tf.nn.dropout(self.conv2, keep_prob_conv)
 
-        self.conv3_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 32, 64),
-                                mean=self.mu, stddev=self.sigma)
+        self.conv3_W = tf.get_variable(
+            name="conv3_w", shape=(3, 3, 32, 64), initializer=initializer
         )
-        self.conv3_b = tf.Variable(tf.zeros(64))
-        self.conv3 = (
-            tf.nn.conv2d(self.conv2, self.conv3_W, strides=[
-                         1, 1, 1, 1], padding="SAME")
-            + self.conv3_b
+
+        self.conv3 = tf.nn.conv2d(
+            self.conv2, self.conv3_W, strides=[1, 1, 1, 1], padding="SAME"
         )
 
         self.conv3 = tf.nn.relu(self.conv3)
 
-        self.conv4_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 64, 64),
-                                mean=self.mu, stddev=self.sigma)
-        )
-        self.conv4_b = tf.Variable(tf.zeros(64))
-        self.conv4 = (
-            tf.nn.conv2d(self.conv3, self.conv4_W, strides=[
-                         1, 1, 1, 1], padding="SAME")
-            + self.conv4_b
+        self.conv4_W = tf.get_variable(
+            name="conv4_w", shape=(3, 3, 64, 64), initializer=initializer
         )
 
+        self.conv4 = tf.nn.conv2d(
+            self.conv3, self.conv4_W, strides=[1, 1, 1, 1], padding="SAME"
+        )
         self.conv4 = tf.nn.relu(self.conv4)
-
         self.conv4 = tf.nn.max_pool(
             self.conv4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID"
         )
         self.conv4 = tf.nn.dropout(self.conv4, keep_prob_conv)  # dropout
 
-        self.conv5_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 64, 128),
-                                mean=self.mu, stddev=self.sigma)
-        )
-        self.conv5_b = tf.Variable(tf.zeros(128))
-        self.conv5 = (
-            tf.nn.conv2d(self.conv4, self.conv5_W, strides=[
-                         1, 1, 1, 1], padding="SAME")
-            + self.conv5_b
+        self.conv5_W = tf.get_variable(
+            name="conv5_w", shape=(3, 3, 64, 128), initializer=initializer
         )
 
+        self.conv5 = tf.nn.conv2d(
+            self.conv4, self.conv5_W, strides=[1, 1, 1, 1], padding="SAME"
+        )
         self.conv5 = tf.nn.relu(self.conv5)
 
-        self.conv6_W = tf.Variable(
-            tf.truncated_normal(shape=(3, 3, 128, 128),
-                                mean=self.mu, stddev=self.sigma)
-        )
-        self.conv6_b = tf.Variable(tf.zeros(128))
-        self.conv6 = (
-            tf.nn.conv2d(self.conv5, self.conv6_W, strides=[
-                         1, 1, 1, 1], padding="SAME")
-            + self.conv6_b
+        self.conv6_W = tf.get_variable(
+            name="conv6_w", shape=(3, 3, 128, 128), initializer=initializer
         )
 
+        self.conv6 = tf.nn.conv2d(
+            self.conv5, self.conv6_W, strides=[1, 1, 1, 1], padding="SAME"
+        )
         self.conv6 = tf.nn.relu(self.conv6)
 
         self.conv6 = tf.nn.max_pool(
@@ -105,32 +94,26 @@ class TrafficSignNet(object):
 
         self.fc0 = tf.reshape(self.conv6, [tf.shape(self.conv6)[0], -1])
 
-        self.fc1_W = tf.Variable(
-            tf.truncated_normal(shape=(2048, 128),
-                                mean=self.mu, stddev=self.sigma)
+        self.fc1_W = tf.get_variable(
+            name="fc1_w", shape=(2048, 128), initializer=initializer
         )
-        self.fc1_b = tf.Variable(tf.zeros(128))
-        self.fc1 = tf.matmul(self.fc0, self.fc1_W) + self.fc1_b
 
+        self.fc1 = tf.matmul(self.fc0, self.fc1_W)
         self.fc1 = tf.nn.relu(self.fc1)
         self.fc1 = tf.nn.dropout(self.fc1, keep_prob)
 
-        self.fc2_W = tf.Variable(
-            tf.truncated_normal(
-                shape=(128, 128), mean=self.mu, stddev=self.sigma)
+        self.fc2_W = tf.get_variable(
+            name="fc2_w", shape=(128, 128), initializer=initializer
         )
-        self.fc2_b = tf.Variable(tf.zeros(128))
-        self.fc2 = tf.matmul(self.fc1, self.fc2_W) + self.fc2_b
+        self.fc2 = tf.matmul(self.fc1, self.fc2_W)
 
         self.fc2 = tf.nn.relu(self.fc2)
         self.fc2 = tf.nn.dropout(self.fc2, keep_prob)
 
-        self.fc3_W = tf.Variable(
-            tf.truncated_normal(shape=(128, n_out),
-                                mean=self.mu, stddev=self.sigma)
+        self.fc3_W = tf.get_variable(
+            name="w", shape=(128, n_out), initializer=initializer
         )
-        self.fc3_b = tf.Variable(tf.zeros(n_out))
-        self.logits = tf.matmul(self.fc2, self.fc3_W) + self.fc3_b
+        self.logits = tf.matmul(self.fc2, self.fc3_W)
 
         self.one_hot_y = tf.one_hot(y, n_out)
         self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
